8675b3f42bb1351a2d54b8864faa005f02b4add1
Theo Drakakis <theo@gemjungle.com>
2025-04-12 11:51:49 +1000

    layout completed

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,11 @@
 import { Application } from "@hotwired/stimulus"
+import Dropdown from '@stimulus-components/dropdown' // Add this line
 
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
+application.register('dropdown', Dropdown) // Add this line
 
 export { application }
